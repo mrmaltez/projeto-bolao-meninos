1dc858599e1a7a84d3d38e9a060ccb0dbad0cc8b
Lucas Soares <lpedrosasoares@gmail.com>
2022-11-08 14:00:01 -0300

    register page

--- bolao/users/views.py
+++ bolao/users/views.py
@@ -1,5 +1,7 @@
+from re import U
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 def register(request):
     if request.method == "GET":
@@ -9,7 +11,15 @@ def register(request):
         email = request.POST.get('email')
         password = request.POST.get('password')
 
-        return HttpResponse('username')
+        user = User.objects.filter(username = username).first()
+
+        if user:
+            return HttpResponse("Usuário já existe")
+        
+        else:
+            user = User.objects.create_user(username, email, password)
+
+        return HttpResponse("Usuário cadastrado com sucesso!")
 
 
 
