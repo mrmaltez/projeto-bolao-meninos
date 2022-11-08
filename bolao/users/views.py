ef16bd12eb9dcbdf704769dd06e9fa9cd78d1a1b
lpedrosasoares <lpedrosasoares@gmail.com>
2022-11-08 13:37:50 -0300

    register page

--- bolao/users/views.py
+++ bolao/users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def register(request):
+    if request.method == "GET":
+        return render(request, 'users/register.html')
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        return HttpResponse('username')
+
+
+
+def login(request):
+    return render(request, 'users/login.html')
