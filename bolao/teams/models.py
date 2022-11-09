a3a2d137be3bd019c1402f46bd1594ea3e93d522
Lucas Soares <lpedrosasoares@gmail.com>
2022-11-09 10:23:45 -0300

    migrate the base

--- bolao/teams/models.py
+++ bolao/teams/models.py
@@ -3,5 +3,7 @@ from django.db import models
 class Teams(models.Model):
     name = models.TextField()
 
+    def __str__(self):
+        return self.name
     class Meta:
         verbose_name_plural = "Times"
