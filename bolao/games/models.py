6f603f858f7813f9f4896998d2694e854d573a5e
Lucas Soares <lpedrosasoares@gmail.com>
2022-11-09 11:45:18 -0300

    jogos

--- bolao/games/models.py
+++ bolao/games/models.py
@@ -5,5 +5,10 @@ class Games(models.Model):
     teamId_1 = models.ForeignKey(Teams, on_delete=models.CASCADE,related_name="teamId_1")
     teamId_2 = models.ForeignKey(Teams, on_delete=models.CASCADE, related_name="teamId_2")
     date = models.DateField()
+
     def __str__(self):
         return f"{self.teamId_1} x  {self.teamId_2}"
+
+    class Meta:
+        verbose_name_plural = "Jogos"
+
