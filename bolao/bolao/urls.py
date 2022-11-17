117cd28b313bbf5ebc48ed27239e4d9ef5400b60
lpedrosasoares <lpedrosasoares@gmail.com>
2022-11-16 22:16:03 -0300

    initial commit

--- bolao/bolao/urls.py
+++ bolao/bolao/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls')),
-    path('games/', include('games.urls'))
+    path('games/', include('games.urls')),
+    path('ranking/', include('raking.urls'))
 ]
